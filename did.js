8226d10b13e5ee1bb5edd23d2e686e5ea79c9f55
quavo <quayyumbello@gmail.com>
2022-09-20 12:07:57 +0100

    first

---
+++ did.js
@@ -0,0 +1,10 @@
+function bee(obj){
+x = (`object name is ${obj.Name}, address is ${obj.Address}, height is ${obj.height} `);
+console.log(x);
+}
+obj = {
+    Name:'Olatunji',
+    Address: 'Akobo',
+    height: 1.5
+}
+// new obj = {}
